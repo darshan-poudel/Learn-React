315d274db0bebf53a3ca6ef96350ec32ffebd49d
Darshan Poudel <darpo@Darshans-MacBook-Pro.local>
2021-05-18 19:00:28 -0500

    first ez example

--- stories/src/App.js
+++ stories/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+let variable = 'variable';
+
+const title = 'React';
+const welcome = {
+  greeting: 'Hey',
+  title: ' React'
+};
+
+function getTitle(title){
+  return title;
+}
 
 function App() {
+  const title = 'React'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello {getTitle('React')} {welcome.greeting}{welcome.title}</h1>
+      <h3>Input field 'label'</h3>
+      <label htmlFor = "search">Search</label>
+      <input id="search" type="text" />
     </div>
   );
 }
