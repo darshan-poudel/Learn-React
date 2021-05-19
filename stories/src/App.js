295f0a48837b9bee510ee4cb27b3cf7a6303582a
Darshan Poudel <darpo@Darshans-MacBook-Pro.local>
2021-05-18 20:26:30 -0500

    Displaying lists

--- stories/src/App.js
+++ stories/src/App.js
@@ -1,26 +1,52 @@
 import './App.css';
 import React from 'react';
 
-let variable = 'variable';
+let purposeCommit = 'Implement a print out of an array';
 
-const title = 'React';
-const welcome = {
-  greeting: 'Hey',
-  title: ' React'
-};
-
-function getTitle(title){
-  return title;
+let fruitArray = ['apple', 'banana','cherry', 'dates'];
+const booklist =[
+  {
+    title: 'Book One',
+    url: 'https://bookone.org/',
+    author: 'author one',
+    num_comments: 3,
+    rating: 4,
+    objectID: 0,
+}, {
+    title: 'Book Two',
+    url: 'https://booktwo.js.org/',
+    author: 'author two',
+    num_comments: 2,
+    rating: 5,
+    objectID: 1,
+}, ];
+function readTitlesFunc(list){
+  list.array.forEach(element => {
+    return <div>{element.title}</div>
+  });
 }
 
 function App() {
   const title = 'React'
   return (
     <div className="App">
-      <h1>Hello {getTitle('React')} {welcome.greeting}{welcome.title}</h1>
-      <h3>Input field 'label'</h3>
-      <label htmlFor = "search">Search</label>
+      <h1>Lets see what happens: <p>{fruitArray.map}</p></h1>
+      <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+      <hr/>
+      <b>Use key values or id's for lists. Issues can be present for dynamic lists causing them to not update.</b>
+      {booklist.map(function(book){
+        return (
+          <div key={book.objectID}>
+            <span>
+              <a href={book.url}>{book.title} </a>
+            </span>
+            <span>Book author: {book.author}</span>
+            <span>Book rating: {book.rating}</span>
+            </div>
+        );
+      })}
+      <hr/>
     </div>
   );
 }
